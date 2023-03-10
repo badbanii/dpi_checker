4aa840447fc5b3e6a705b7e28bf367007e2c6f2a
Tulitu Vlad <tulituvlad@gmail.com>
2023-03-10 11:49:31 +0200

    Added fragments

---
+++ app/src/main/java/com/theviciousgames/dpichecker/ui/home/view/HomeFragment.kt
@@ -0,0 +1,10 @@
+package com.theviciousgames.dpichecker.ui.home.view
+
+import androidx.fragment.app.Fragment
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.theviciousgames.dpichecker.R
+import com.theviciousgames.dpichecker.databinding.FragmentHomeBinding
+
+class HomeFragment:Fragment(R.layout.fragment_home) {
+    private val binding by viewBinding(FragmentHomeBinding::bind)
+}
---
+++ app/src/main/java/com/theviciousgames/dpichecker/ui/onboarding/view/OnBoardingFragment.kt
@@ -0,0 +1,11 @@
+package com.theviciousgames.dpichecker.ui.onboarding.view
+
+import androidx.fragment.app.Fragment
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.theviciousgames.dpichecker.R
+import com.theviciousgames.dpichecker.databinding.FragmentHomeBinding
+import com.theviciousgames.dpichecker.databinding.FragmentOnboardingBinding
+
+class OnBoardingFragment: Fragment(R.layout.fragment_onboarding) {
+    private val binding by viewBinding(FragmentOnboardingBinding::bind)
+}
