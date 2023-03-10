1d9f6eb93db5e4d8f276e29293852d1bfe568d36
Tulitu Vlad <tulituvlad@gmail.com>
2023-03-10 13:23:24 +0200

    Implemented navigation

--- app/src/main/java/com/theviciousgames/dpichecker/ui/home/view/HomeFragment.kt
+++ app/src/main/java/com/theviciousgames/dpichecker/ui/home/view/HomeFragment.kt
@@ -1,10 +1,26 @@
 package com.theviciousgames.dpichecker.ui.home.view
 
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.theviciousgames.dpichecker.R
 import com.theviciousgames.dpichecker.databinding.FragmentHomeBinding
+import com.theviciousgames.dpichecker.utils.Destinations
 
 class HomeFragment:Fragment(R.layout.fragment_home) {
     private val binding by viewBinding(FragmentHomeBinding::bind)
+
+    private fun navigateTo(destination: Destinations)
+    {
+        when(destination)
+        {
+            Destinations.Menu->{
+                if (findNavController().currentDestination?.id == R.id.homeFragment)
+                {
+                    findNavController().navigate(R.id.action_homeFragment_to_menuFragment)
+                }
+            }
+            else -> {}
+        }
+    }
 }
