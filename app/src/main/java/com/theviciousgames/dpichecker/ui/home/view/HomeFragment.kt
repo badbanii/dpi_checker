fb8f4e8952f27f31a5db355649a5ed1cdf62f209
Tulitu Vlad <tulituvlad@gmail.com>
2023-03-13 18:46:13 +0200

    Updated UI

--- app/src/main/java/com/theviciousgames/dpichecker/ui/home/view/HomeFragment.kt
+++ app/src/main/java/com/theviciousgames/dpichecker/ui/home/view/HomeFragment.kt
@@ -1,10 +1,7 @@
 package com.theviciousgames.dpichecker.ui.home.view
 
 import android.os.Bundle
-import android.util.Log
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -14,53 +11,46 @@ import com.theviciousgames.dpichecker.databinding.FragmentHomeBinding
 import com.theviciousgames.dpichecker.ui.home.viewmodel.HomeViewModel
 import com.theviciousgames.dpichecker.utils.Destinations
 
-class HomeFragment:Fragment(R.layout.fragment_home) {
+class HomeFragment : Fragment(R.layout.fragment_home) {
     private val binding by viewBinding(FragmentHomeBinding::bind)
     private val viewModel: HomeViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
         updateUi()
+        super.onViewCreated(view, savedInstanceState)
     }
 
-    private fun updateUi()
-    {
-        with(binding)
-        {
-            textviewValueDensity.text= getCurrentDensity().toString()
-        }
+    private fun updateUi() {
+        binding.textviewValueDensity.text = getCurrentDensity().toString()
+        binding.textviewValueScaledDensity.text = getCurrentScaledDensity().toInt().toString()
+        binding.textviewValueXDensity.text = getCurrentXDensity().toInt().toString()
+        binding.textviewValueYDensity.text = getCurrentYDensity().toInt().toString()
     }
-    private fun getCurrentDensity():Int
-    {
+
+    private fun getCurrentDensity(): Int {
         return viewModel.getCurrentDensity(requireActivity())
     }
 
-    private fun getCurrentXDensity()
-    {
-        Log.d("debug", viewModel.getCurrentXDensity(requireActivity()).toString())
+    private fun getCurrentXDensity(): Float {
+        return viewModel.getCurrentXDensity(requireActivity())
     }
 
-    private fun getCurrentYDensity()
-    {
-        Log.d("debug", viewModel.getCurrentYDensity(requireActivity()).toString())
+    private fun getCurrentYDensity(): Float {
+        return viewModel.getCurrentYDensity(requireActivity())
     }
 
-    private fun getCurrentScaledDensity()
-    {
-        Log.d("debug", viewModel.getCurrentScaledDensity(requireActivity()).toString())
+    private fun getCurrentScaledDensity(): Float {
+        return viewModel.getCurrentScaledDensity(requireActivity())
     }
 
-    private fun navigateTo(destination: Destinations)
-    {
-        when(destination)
-        {
-            Destinations.Menu->{
-                if (findNavController().currentDestination?.id == R.id.homeFragment)
-                {
+    private fun navigateTo(destination: Destinations) {
+        when (destination) {
+            Destinations.Menu -> {
+                if (findNavController().currentDestination?.id == R.id.homeFragment) {
                     findNavController().navigate(R.id.action_homeFragment_to_menuFragment)
                 }
             }
+
             else -> {}
         }
     }
--- app/src/main/java/com/theviciousgames/dpichecker/ui/menu/view/MenuFragment.kt
+++ app/src/main/java/com/theviciousgames/dpichecker/ui/menu/view/MenuFragment.kt
@@ -1,11 +1,6 @@
 package com.theviciousgames.dpichecker.ui.menu.view
 
 import androidx.fragment.app.Fragment
-import by.kirich1409.viewbindingdelegate.viewBinding
 import com.theviciousgames.dpichecker.R
-import com.theviciousgames.dpichecker.databinding.FragmentHomeBinding
-import com.theviciousgames.dpichecker.databinding.FragmentMenuBinding
 
-class MenuFragment: Fragment(R.layout.fragment_menu) {
-    private val binding by viewBinding(FragmentMenuBinding::bind)
-}
+class MenuFragment : Fragment(R.layout.fragment_menu)
